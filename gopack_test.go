19803b9d34e63011b87185bfc1663734354701c1
Benoit Sigoure <tsunanet@gmail.com>
2015-10-16 04:21:54 -0700

    Fix an integer overflow during packer construction.
    The offset to the lsb was expressed as a uint8, which only allowed the
    code to cover 256 bits (32 bytes) of input.  Beyond that the code would
    erroneously wrap around in the input buffer, as shown by the regression
    test added in this commit.

--- gopack_test.go
+++ gopack_test.go
@@ -30,3 +30,24 @@ func testPackUnpack(t *testing.T) {
 		t.Fatalf("Expected %v; got %v", typ{255}, t1)
 	}
 }
+
+func TestLargeValue(t *testing.T) {
+	type big struct {
+		V1, V2, V3, V4, V5, V6, V7, V8 uint32 // 32 bytes
+
+		B uint8
+	}
+
+	b := big{B: 42}
+	bytes := make([]byte, 33)
+	Pack(bytes, &b)
+	if bytes[0] != 0 {
+		t.Errorf("First byte should have been 0 but was %v", bytes[0])
+	}
+
+	b2 := big{}
+	Unpack(bytes, &b2)
+	if b != b2 {
+		t.Fatalf("Expected %#v; got %#v", b, b2)
+	}
+}
